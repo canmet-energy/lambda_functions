4b8efc0a56007e1c17b255e8fd9b66280d1570cf
Chris Kirney <chris.kirney@canada.ca>
2019-08-16 13:14:03 +0000

    Finishing code to compile individual simulations_#.json or error_#.json file in to one file.

--- append_BTAP_results/append_BTAP_results.rb
+++ append_BTAP_results/append_BTAP_results.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'aws-sdk-s3'
 require 'json'
 
@@ -12,97 +11,21 @@ def handler(event:, context:)
 end
 
 def process_results(osa_id:, bucket_name:, append_tag:, cycle_count:)
-  region = 'us-east-1'
-  s3 = Aws::S3::Resource.new(region: region)
-  s3_client = AW::S3::Client.new(region: region)
-  bucket = s3.bucket(bucket_name)
+  res_comp = []
   for result_num in 1..cycle_count
-    puts 'hello'
-    res_tag = osa_id + '/' + append_tag + '_' + result_num.to_s + '.json'
-    res_obj = bucket.object(res_tag)
-    if res_obj.exists?
-      res_stream = s3_client.body.read
-    end
-  end
-
-  object_keys.each do |object_key|
-    #If you find an osw.zip file try downloading it and adding the information to the error_col array of hashes.
-    string_start = osa_id.size + 1
-    osd_id = object_key[string_start..-5]
-    qaqc_col, error_col = process_file(osa_id: osa_id, osd_id: osd_id, file_id: object_key, analysis_json: analysis_json, bucket_name: bucket_name, qaqc_col: qaqc_col, error_col: error_col)
-    if qaqc_col == false
-      return error_col
-    end
-  end
-
-  out_count = (cycle_count.to_i + 1).to_s
-
-  qaqc_col_file = osa_id.to_s + "/" + "simulations_" + out_count + ".json"
-  err_col_file = osa_id.to_s + "/" + "error_col_" + out_count + ".json"
-  qaqc_col_data = get_s3_stream(file_id: qaqc_col_file, bucket_name: bucket_name)
-  qaqc_col_data << qaqc_col
-  err_col_data = get_s3_stream(file_id: err_col_file, bucket_name: bucket_name)
-  err_col_data << error_col
-  qaqc_status = put_data_s3(file_id: qaqc_col_file, bucket_name: bucket_name,data: qaqc_col_data)
-  err_status = put_data_s3(file_id: err_col_file, bucket_name: bucket_name,data: err_col_data)
-  return true
-end
-
-def process_file(osa_id:, osd_id:, file_id:, analysis_json:, bucket_name:, qaqc_col:, error_col:)
-  if file_id.nil?
-    return "No file name passed."
-  else
-    s3file = get_file_s3(file_id: file_id, bucket_name: bucket_name)
-  end
-  if s3file[:exist]
-    osw_json = unzip_osw(zip_file: s3file[:file])
-  else
-    file_exist = false
-    message = "Could not find #{file_id}"
-    return file_exist, message
-  end
-  osw_json.each do |osw|
-    aid = osw['osa_id']
-    uuid = osw['osd_id']
-    if aid.nil? || uuid.nil?
-      puts "Error either aid: #{aid} or uuid: #{uuid} not present"
+    res_key = osa_id + '/' + append_tag + '_' + result_num.to_s + '.json'
+    res_json = get_s3_stream(file_id: res_key, bucket_name: bucket_name)
+    if res_json.empty? || res_json.nil?
+      return "Could not get object with key #{res_key} in bucket #{bucket_name}."
     else
-      qaqc, error_info = extract_data_from_osw(osw_json: osw, uuid: uuid, aid: aid, analysis_json: analysis_json)
-      qaqc.each do |qaqc_ind|
-        qaqc_col << qaqc_ind
-      end
-      error_info.each do |error_ind|
-        error_col << error_ind
+      res_json.each do |ind_res|
+        res_comp << ind_res
       end
     end
   end
-  # Get rid of the datapoint osw file that was just downloaded.
-  File.delete(s3file[:file])
-  return qaqc_col, error_col
-end
-
-def get_file_s3(file_id:, bucket_name:)
-  region = 'us-east-1'
-  s3 = Aws::S3::Resource.new(region: region)
-  bucket = s3.bucket(bucket_name)
-  ret_bucket = bucket.object(file_id)
-  if ret_bucket.exists?
-    #If you find an osw.zip file try downloading it and adding the information to the error_col array of hashes.
-    download_loc = '/tmp/out.zip'
-    osw_index = 0
-    while osw_index < 10
-      osw_index += 1
-      ret_bucket.download_file(download_loc)
-      osw_index = 11 if File.exist?(download_loc)
-    end
-    if osw_index == 10
-      return {exist: false, file: nil}
-    else
-      return {exist: true, file: download_loc}
-    end
-  else
-    return {exist: false, file: nil}
-  end
+  out_key = osa_id + '/' + append_tag + '.json'
+  res_out = put_data_s3(file_id: out_key, bucket_name: bucket_name, data: res_comp)
+  return res_out
 end
 
 def get_s3_stream(file_id:, bucket_name:)
@@ -125,5 +48,7 @@ def put_data_s3(file_id:, bucket_name:, data:)
   s3 = Aws::S3::Resource.new(region: region)
   bucket = s3.bucket(bucket_name)
   out_obj = bucket.object(file_id)
-  out_obj.put(body: out_data)
+  while out_obj.exists? == false
+    out_obj.put(body: out_data)
+  end
 end
