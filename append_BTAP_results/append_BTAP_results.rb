cc465917c8b459e54086f7a803ced511254cdd16
Chris Kirney <chris.kirney@canada.ca>
2019-10-30 18:56:19 +0000

    Rather than transforming the json files into an array of hashes first, 'append_BTAP_results' now just modifies the strings slightly and sticks them together before saving them to S3 again.

--- append_BTAP_results/append_BTAP_results.rb
+++ append_BTAP_results/append_BTAP_results.rb
@@ -14,46 +14,23 @@ def handler(event:, context:)
   unless event["region"].nil?
     aws_region = event["region"]
   end
-  response = process_results(osa_id: osa_id, bucket_name: bucket_name, append_tag: append_tag, cycle_count: cycle_count, analysis_json: analysis_json, region: aws_region)
+  s3_cli = Aws::S3::Client.new(region: aws_region)
+  response = process_results(osa_id: osa_id, bucket_name: bucket_name, append_tag: append_tag, cycle_count: cycle_count, analysis_json: analysis_json, region: aws_region, s3_cli: s3_cli)
   { statusCode: 200, body: JSON.generate(response) }
 end
 
-def process_results(osa_id:, bucket_name:, append_tag:, cycle_count:, analysis_json:, region:)
-  res_comp = []
+def process_results(osa_id:, bucket_name:, append_tag:, cycle_count:, analysis_json:, region:, s3_cli:)
+  res_comp = "["
   for result_num in 1..cycle_count
     res_key = analysis_json[:analysis_name] + '_' + osa_id + '/' + append_tag + '_' + result_num.to_s + '.json'
-    res_json = get_s3_stream(file_id: res_key, bucket_name: bucket_name, region: region)
-    if res_json.empty? || res_json.nil?
-      return "Could not get object with key #{res_key} in bucket #{bucket_name} in region #{region}."
-    else
-      res_comp.concat(res_json)
-    end
+    res_comp << s3_cli.get_object(bucket: bucket_name, key: res_key).body.read[1..-2] + ','
   end
+  res_comp[-1] = ']'
   out_key = analysis_json[:analysis_name] + '_' + osa_id + '/' + append_tag + '.json'
-  res_out = put_data_s3(file_id: out_key, bucket_name: bucket_name, data: res_comp, region: region)
-  return res_out
-end
-
-def get_s3_stream(file_id:, bucket_name:, region:)
-  s3_res = Aws::S3::Resource.new(region: region)
-  bucket = s3_res.bucket(bucket_name)
-  ret_bucket = bucket.object(file_id)
-  if ret_bucket.exists?
-    s3_cli = Aws::S3::Client.new(region: region)
-    return_data = JSON.parse(s3_cli.get_object(bucket: bucket_name, key: file_id)[:body].string)
-  else
-    return_data = []
-  end
-  return return_data
-end
-
-def put_data_s3(file_id:, bucket_name:, data:, region:)
-  out_data = JSON.generate(data)
-  s3 = Aws::S3::Resource.new(region: region)
-  bucket = s3.bucket(bucket_name)
-  out_obj = bucket.object(file_id)
-  while out_obj.exists? == false
-    res = out_obj.put(body: out_data)
-  end
-  return res
+  resp = s3_cli.put_object({
+                               body: res_comp,
+                               bucket: bucket_name,
+                               key: out_key
+                           })
+  return resp
 end
