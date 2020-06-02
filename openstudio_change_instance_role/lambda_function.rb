f06b7d6602bfe4f78f041f6664aa8903621773f1
Chris Kirney <chris.kirney@canada.ca>
2020-06-02 16:30:50 +0000

    Including AMI IDs for OpenStudio-Server-Docker-2.8.1-4 and OpenStudio-Server-Docker-3.0.0.

--- openstudio_change_instance_role/lambda_function.rb
+++ openstudio_change_instance_role/lambda_function.rb
@@ -45,7 +45,7 @@ def lambda_handler(event:, context:)
   ami_id = instance_info.reservations[0].instances[0].image_id
   iam_profile_info = instance_info.reservations[0].instances[0].iam_instance_profile
   test_arn = "arn:aws:iam::237788425317:instance-profile/ec2_s3_lambda"
-  test_ami_ids = ["ami-095aa48abe1410bb2", "ami-0556ba62cb366627a"]
+  test_ami_ids = ["ami-095aa48abe1410bb2", "ami-0556ba62cb366627a", "ami-088db32fa4a7ad506", "ami-037ca03cca51aef46"]
   test_ami_id = false
   test_ami_ids.each do |check_ami_id|
     if check_ami_id == ami_id
